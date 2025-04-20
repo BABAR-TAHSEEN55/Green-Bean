eac9ccde053eb75d200876d8a49ea484f4b397ac
Babar <babarsmoke24@gmail.com>
2025-04-20 19:20:48 +0530

    feat : HeroSection added and Implemented

---
+++ src/Components/HeroSection.tsx
@@ -0,0 +1,15 @@
+const HeroSection = () => {
+  return (
+    // <div className="container h-[496px]">
+    <div className="container lg:px-0 lg:max-w-none h-[500px] mt-20 ">
+      <h1 className="lg:text-[300px]  font-bai   leading-none text-center tracking-tight font-semibold  uppercase">
+        Art
+      </h1>
+      <h1 className="lg:text-[300px] font-bai  leading-none text-center tracking-tight font-semibold  uppercase">
+        Objects
+      </h1>
+    </div>
+  );
+};
+
+export default HeroSection;
