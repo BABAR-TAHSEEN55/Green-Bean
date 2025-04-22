fd35d9d13f1dd607f7040f0be7253bb0f53ed127
Babar <babarsmoke24@gmail.com>
2025-04-22 11:37:51 +0530

    style : Height tweaks

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -1,7 +1,7 @@
 const HeroSection = () => {
   return (
     // <div className="container h-[496px]">
-    <div className="container lg:px-0 lg:max-w-none h-[500px] mt-20 ">
+    <div className="container lg:px-0 lg:max-w-none h-[300px] md:h-[500px] lg:h-[800px] mt-20 ">
       <h1 className="lg:text-[300px]  font-bai   leading-none text-center tracking-tight font-semibold  uppercase">
         Art
       </h1>
@@ -13,3 +13,4 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
+//Leading none takes removes all the unnecessary spaces between the words`
