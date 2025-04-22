3ea96a24d6430fe879e93dcdee6739c3253c45b5
Babar <babarsmoke24@gmail.com>
2025-04-22 11:38:09 +0530

    feat : About section Created

---
+++ src/Components/About.tsx
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div className="container mt-8 text-center ">
+      <p className="text-2xl lg:text-[30px] font-mono font-semibold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+        similique exercitationem. Atque nostrum laboriosam praesentium? Quos, ut
+        inventore eum atque corporis iusto adipisci hic nemo debitis ullam,
+        error, earum architecto?
+      </p>
+    </div>
+  );
+};
+
+export default About;
