d9a358ba2a4ad792f419d5f2b26ff3d558871467
Babar <babarsmoke24@gmail.com>
2025-04-22 11:37:42 +0530

    chore : Constants added

---
+++ src/constants/constants.ts
@@ -0,0 +1,30 @@
+const PHOTOS: string[] = [
+  "https://images.unsplash.com/photo-1598838073192-05c942ede858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=100",
+  "https://images.unsplash.com/photo-1552248524-10d9a7e4841c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=881&q=100",
+];
+//TODO:make it into array of obj and add titles also create interface for it as well
+export { PHOTOS };
+
+// const PHOTOS = [
+//   {
+//     src: "https://images.unsplash.com/photo-1",
+//     type: "profile",
+//   },
+//   {
+//     src: "https://images.unsplash.com/photo-2",
+//     type: "banner",
+//   },
+// ];
+// {PHOTOS.map(({ src, type }, index) => (
+//   <img
+//     key={index}
+//     src={src}
+//     alt={type}
+//     className={
+//       type === "profile"
+//         ? "w-32 h-32 rounded-full object-cover"
+//         : "w-full h-48 object-cover"
+//     }
+//   />
+// ))}
+// For Array of object you can map and get the index value like this
