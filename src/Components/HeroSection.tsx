94d23ba467b3f9fe6606e86e53658bbb681330b5
Babar <babarsmoke24@gmail.com>
2025-04-27 15:49:58 +0530

    chore : Data scroll added for Smooth Scroll

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -1,13 +1,27 @@
+import { motion } from "framer-motion";
 const HeroSection = () => {
   return (
     // <div className="container h-[496px]">
-    <div className="container lg:px-0 lg:max-w-none h-[300px] md:h-[500px] lg:h-[800px] mt-20 ">
-      <h1 className="lg:text-[300px]  font-bai   leading-none text-center tracking-tight font-semibold  uppercase">
+    <div
+      className="container lg:px-0 lg:max-w-none h-[300px] md:h-[500px] lg:h-[800px] mt-20 "
+      data-scroll-container
+    >
+      <motion.h1
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+        className="lg:text-[300px] text-[20vw] font-bai   leading-none text-center tracking-tight font-semibold  uppercase"
+      >
         Art
-      </h1>
-      <h1 className="lg:text-[300px] font-bai  leading-none text-center tracking-tight font-semibold  uppercase">
+      </motion.h1>
+      <motion.h1
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+        className="lg:text-[300px] font-bai text-[20vw] leading-none text-center tracking-tight font-semibold  uppercase"
+      >
         Objects
-      </h1>
+      </motion.h1>
     </div>
   );
 };
